ba865eff4b8b8633a3d009a3b651aaf184c4658b
James Burger <helpingofburgers@gmail.com>
2017-10-24 11:57:25 -0400

    We've added a second line

--- src/DemoGit2.java
+++ src/DemoGit2.java
@@ -2,7 +2,8 @@
 public class DemoGit2 {
 
 	public static void main(String[] args) {
-	System.out.println("Hello World Again!");
+		System.out.println("Hello World Again!");
+		System.out.println("Hello World Two!");
 	}
 
 }
